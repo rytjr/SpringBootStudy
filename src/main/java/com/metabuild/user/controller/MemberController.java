212f53a72e8d749015ccc531009b7b21d80b29ae
rytjr <kks007159@naver.com>
2024-12-13 19:21:26 +0900

    admin page make

--- src/main/java/com/metabuild/user/controller/MemberController.java
+++ src/main/java/com/metabuild/user/controller/MemberController.java
@@ -6,10 +6,7 @@ import jakarta.servlet.http.HttpSession;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import jakarta.annotation.Resource;
 import java.util.List;
@@ -127,6 +124,11 @@ public class MemberController {
         model.addAttribute("user", user);
 
         return "/auth/myPage";
+    }
+
+    @PostMapping("/auth/mypage")
+    public String postMyPage(){
+        return "message";
     }
 
 }
