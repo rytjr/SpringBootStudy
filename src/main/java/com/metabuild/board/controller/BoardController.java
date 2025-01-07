1ef40b16b47d365acd0107fee8649f20bb0ced46
rytjr <kks007159@naver.com>
2025-01-07 09:02:54 +0900

    admin page make

--- src/main/java/com/metabuild/board/controller/BoardController.java
+++ src/main/java/com/metabuild/board/controller/BoardController.java
@@ -36,6 +36,7 @@ public class BoardController {
     public String boardForm() {
 
         log.info("uploadDIr == {}",uploadDir);
+        log.info("uploadDIr == {}",uploadDir);
 
         return "board/write";
     }
